8f82efbf590e80c2f91b65f6609571164b7f366b
prizx <122016035+prizx@users.noreply.github.com>
2023-01-05 21:22:36 +1030

    Update prizx.lua

--- prizx.lua
+++ prizx.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("ANAL GUI", "DarkTheme")
+local Window = Library.CreateLib("ANAL GUI - prizx#0001", "DarkTheme")
 
 -- Main
 local Main = Window:NewTab("Main")
